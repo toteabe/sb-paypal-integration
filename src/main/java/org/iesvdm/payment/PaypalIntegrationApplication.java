5086326095d2dd2653c57937833ed5ed9ea4e343
toteabe <toteabe@yahoo.es>
2025-04-10 08:20:50 +0200

    inicial sb thymeleaf BS

---
+++ src/main/java/org/iesvdm/payment/PaypalIntegrationApplication.java
@@ -0,0 +1,11 @@
+package org.iesvdm.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalIntegrationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/org/iesvdm/payment/PaypalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesvdm.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
